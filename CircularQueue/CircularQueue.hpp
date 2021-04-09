5dc876f332994788a542e28d244448d84ff36e9c
NoHandsMate <z170s.lol@gmail.com>
2021-04-09 17:07:56 +0200

    Completed Circular Queue

--- CircularQueue/CircularQueue.hpp
+++ CircularQueue/CircularQueue.hpp
@@ -6,7 +6,10 @@
 #define DATASTRUCTURES_CIRCULARQUEUE_HPP
 
 #include <array>
+#include <optional>
+#include <iostream>
 
+//TODO adjust const correctness and noexcept.
 
 constexpr static int MAX_SIZE = 5;
 
@@ -14,9 +17,71 @@ template<typename T>
 class CircQueue {
 
 public:
+    CircQueue()
+        : head(-1), tail(-1) {}
+
+    constexpr bool isEmpty() const {
+       return (tail == -1);
+    }
+
+    constexpr bool isFull() const {
+
+        if(tail == MAX_SIZE - 1) {
+            return true;
+        } else if(head == tail + 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    constexpr void enQueue(const T&& item) {
+
+        if(!isFull()) {
+
+            head = isEmpty() ? 0 : head;
+
+            tail = (tail + 1) % MAX_SIZE;
+            circ_queue[tail] = item;
+
+        } else {
+            std::cout << "Cannot enQueue: queue is full" << '\n';
+        }
+    }
+
+    constexpr std::optional<T> deQueue() {
+
+        if(!isEmpty()) {
+
+            auto result = circ_queue[head];
+
+            if(head == tail) {
+                head = -1;
+                tail = -1;
+            } else {
+
+                head = (head + 1) % MAX_SIZE;
+                return result;
+            }
+
+        } else {
+            std::cout << "Cannot deQueue: queue is empty" << '\n';
+        }
+
+        return std::nullopt;
+    }
+
+    constexpr void printQueue() {
+        for(std::size_t i = head; i < MAX_SIZE; i++) {
+            std::cout << circ_queue[i] << " ";
+        }
+        std::cout << '\n';
+    }
 
 private:
-    std::array<
+    std::array<T, MAX_SIZE> circ_queue{};
+    int head;
+    int tail;
 };
 
 
--- CircularQueue/TestCircQueue.cpp
+++ CircularQueue/TestCircQueue.cpp
@@ -2,7 +2,29 @@
 // Created by lorenzo on 07/04/21.
 //
 
+#include "CircularQueue.hpp"
 
 int main() {
+
+    CircQueue<int> c_queue;
+
+    c_queue.enQueue(1);
+    c_queue.enQueue(2);
+    c_queue.enQueue(3);
+    c_queue.enQueue(4);
+    c_queue.enQueue(5);
+
+    c_queue.printQueue();
+
+    auto a = c_queue.deQueue();
+    auto b = c_queue.deQueue();
+    auto c = c_queue.deQueue();
+
+    std::cout << *a << '\n';
+    std::cout << *b << '\n';
+    std::cout << *c << '\n';
+
+    c_queue.printQueue();
+
     return 0;
 }
